771b6a227c7da26f0151e27e78604416e2f113ff
Abubakar Tanko <abubakartanko99@gmail.com>
2024-03-29 14:02:20 +0100

    Version 1

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const aside = document.querySelector("aside");
 
   toggleAside.addEventListener("click", function () {
-    aside.classList.toggle("hidden");
+    const navLinks = document.querySelectorAll(".nav-links a");
+    navLinks.forEach(function (link) {
+      const span = link.querySelector("span");
+      span.classList.toggle("hidden");
+    });
   });
 
   const navLinks = document.querySelectorAll(".nav-links a");
   navLinks.forEach(function (link) {
-    const spanText = link.querySelector("span").textContent;
+    const span = link.querySelector("span");
+    const spanText = span.textContent;
     link.setAttribute("title", spanText);
   });
 });
