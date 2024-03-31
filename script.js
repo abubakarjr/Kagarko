85739b0fcb9431b0eb8c6174f01673912ec99ea0
Abubakar Tanko <abubakartanko99@gmail.com>
2024-03-31 23:37:26 +0100

    Version 1

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const aside = document.querySelector("aside");
 
   toggleAside.addEventListener("click", function () {
+    // Toggle visibility of spans within nav-links
     const navLinks = document.querySelectorAll(".nav-links a");
     navLinks.forEach(function (link) {
       const span = link.querySelector("span");
@@ -10,10 +11,28 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
+  // Iterate through each nav link
   const navLinks = document.querySelectorAll(".nav-links a");
   navLinks.forEach(function (link) {
     const span = link.querySelector("span");
     const spanText = span.textContent;
     link.setAttribute("title", spanText);
+
+    // Hide span by default
+    span.classList.add("hidden");
+
+    // Add click event listener to each nav link
+    link.addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent default anchor behavior
+
+      const targetId = this.getAttribute("title")
+        .toLowerCase()
+        .replace(/ /g, "-");
+      const targetCard = document.getElementById(targetId);
+
+      if (targetCard) {
+        targetCard.scrollIntoView({ behavior: "smooth" });
+      }
+    });
   });
 });
